76c449058bc97980de3b29cb2a09430efe90e6dd
kkyu12 <101681296+kkyu12@users.noreply.github.com>
2022-03-28 18:02:59 +0900

    Multiplication Table 4

---
+++ Report2.java
@@ -0,0 +1,68 @@
+package Mar26th;
+
+import java.util.Scanner;
+
+public class Report2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int num1 = scanner.nextInt();
+		int i = 2;
+		switch (num1) {
+		case 2:
+			while (i < 10) {
+				System.out.println(num1 + " X " + i + " = " + num1 * i);
+				i++;
+			}
+			break;
+		case 3:
+			while (i < 10) {
+				System.out.println(num1 + " X " + i + " = " + num1 * i);
+				i++;
+			}
+			break;
+		case 4:
+			while (i < 10) {
+				System.out.println(num1 + " X " + i + " = " + num1 * i);
+				i++;
+			}
+			break;
+		case 5:
+			while (i < 10) {
+				System.out.println(num1 + " X " + i + " = " + num1 * i);
+				i++;
+			}
+			break;
+		case 6:
+			while (i < 10) {
+				System.out.println(num1 + " X " + i + " = " + num1 * i);
+				i++;
+			}
+			break;
+		case 7:
+			while (i < 10) {
+				System.out.println(num1 + " X " + i + " = " + num1 * i);
+				i++;
+			}
+			break;
+		case 8:
+			while (i < 10) {
+				System.out.println(num1 + " X " + i + " = " + num1 * i);
+				i++;
+			}
+			break;
+		case 9:
+			while (i < 10) {
+				System.out.println(num1 + " X " + i + " = " + num1 * i);
+				i++;
+			}
+			break;
+		default:
+			System.out.println("Error");
+			break;
+		}
+
+	}
+
+}
