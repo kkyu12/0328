4ea3f61d1db295267b88ec32e29ba869a85c132a
kkyu12 <101681296+kkyu12@users.noreply.github.com>
2022-03-28 18:33:43 +0900

    Multiplication Table (switch)

---
+++ Practice14.java
@@ -0,0 +1,59 @@
+package Mar28th;
+
+import java.util.Scanner;
+
+public class Practice14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("What number do you want?");
+		int num1 = scanner.nextInt();
+		switch (num1) {
+		case 2:
+			for (int i = 2; i <= 9; i++) {
+				System.out.println(num1 + " X " + i + " = " + num1 * i);
+			}
+			break;
+		case 3:
+			for (int i = 2; i <= 9; i++) {
+				System.out.println(num1 + " X " + i + " = " + num1 * i);
+			}
+			break;
+		case 4:
+			for (int i = 2; i <= 9; i++) {
+				System.out.println(num1 + " X " + i + " = " + num1 * i);
+			}
+			break;
+		case 5:
+			for (int i = 2; i <= 9; i++) {
+				System.out.println(num1 + " X " + i + " = " + num1 * i);
+			}
+			break;
+		case 6:
+			for (int i = 2; i <= 9; i++) {
+				System.out.println(num1 + " X " + i + " = " + num1 * i);
+			}
+			break;
+		case 7:
+			for (int i = 2; i <= 9; i++) {
+				System.out.println(num1 + " X " + i + " = " + num1 * i);
+			}
+			break;
+		case 8:
+			for (int i = 2; i <= 9; i++) {
+				System.out.println(num1 + " X " + i + " = " + num1 * i);
+			}
+			break;
+		case 9:
+			for (int i = 2; i <= 9; i++) {
+				System.out.println(num1 + " X " + i + " = " + num1 * i);
+			}
+			break;
+		default:
+			System.out.println("Error");
+		}
+
+	}
+
+}
