fd5f00d029214f3603ec55c98a3069ca888031b7
kkyu12 <101681296+kkyu12@users.noreply.github.com>
2022-03-28 18:25:10 +0900

    Base 10 to Base 3 Converter

---
+++ Practice3.java
@@ -0,0 +1,19 @@
+package Mar28th;
+
+import java.util.Scanner;
+
+public class Practice3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int num1 = scanner.nextInt();
+		System.out.print(num1 % 3);
+		num1 = num1 / 3;
+		System.out.print(num1 % 3);
+		num1 = num1 / 3;
+		System.out.print(num1 % 3);
+
+	}
+
+}
