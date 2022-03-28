668b1aebe9465da8ba44eae8bf9474c66797b973
kkyu12 <101681296+kkyu12@users.noreply.github.com>
2022-03-28 18:29:45 +0900

    if ... else if ... else (Condition)

---
+++ Practice7.java
@@ -0,0 +1,23 @@
+package Mar28th;
+
+import java.util.Scanner;
+
+public class Practice7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		if (n == 1) {
+			System.out.println("One");
+		} else if (n == 2) {
+			System.out.println("Two");
+		} else if (n == 3) {
+			System.out.println("Three");
+		} else {
+			System.out.println("Except 1, 2, 3");
+		}
+
+	}
+
+}
