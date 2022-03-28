f9d0d0eed7d24292d33045707604f25733c3e6b6
kkyu12 <101681296+kkyu12@users.noreply.github.com>
2022-03-28 18:39:05 +0900

    while & do ... while (Loop)

---
+++ Practice16.java
@@ -0,0 +1,25 @@
+package Mar28th;
+
+public class Practice16 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		while (true) {
+			System.out.println(i);
+			if (i == 100) {
+				break;
+			}
+			i++;
+			
+		}
+		
+		i = 0;
+		do {
+			System.out.println(i);
+			i++;
+		} while (i <= 0);
+
+	}
+
+}
