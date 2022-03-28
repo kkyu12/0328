3307b2fa1e14d34230a6a136be655b28f95d1e8d
kkyu12 <101681296+kkyu12@users.noreply.github.com>
2022-03-28 18:25:58 +0900

    Draw a Right Triangle

---
+++ Report3.java
@@ -0,0 +1,20 @@
+package Mar28th;
+
+import java.util.Scanner;
+
+public class Report3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int num1 = scanner.nextInt();
+		for (int i = 1; i <= num1; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
