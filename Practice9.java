9e845ac91bf66b8671642904311be3183cdf2297
kkyu12 <101681296+kkyu12@users.noreply.github.com>
2022-03-28 18:31:13 +0900

    Multiplication Table

---
+++ Practice9.java
@@ -0,0 +1,27 @@
+package Mar28th;
+
+import java.util.Scanner;
+
+public class Practice9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("What number do you want?");
+		int num1 = scanner.nextInt();
+		System.out.println(num1 + " X 2 = " + num1 * 2);
+		System.out.println(num1 + " X 3 = " + num1 * 3);
+		System.out.println(num1 + " X 4 = " + num1 * 4);
+		System.out.println(num1 + " X 5 = " + num1 * 5);
+		System.out.println(num1 + " X 6 = " + num1 * 6);
+		System.out.println(num1 + " X 7 = " + num1 * 7);
+		System.out.println(num1 + " X 8 = " + num1 * 8);
+		System.out.println(num1 + " X 9 = " + num1 * 9);
+
+		/*
+		 * for (int i=2;i<=9;i++) { System.out.printf("%d X %d = %d\n",num1,i,num1*i); }
+		 */
+
+	}
+
+}
