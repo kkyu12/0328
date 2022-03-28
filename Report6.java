618910776fac81b03a49415ad9dcf500b72b5651
kkyu12 <101681296+kkyu12@users.noreply.github.com>
2022-03-28 18:05:04 +0900

    Find the Factors of N

---
+++ Report6.java
@@ -0,0 +1,20 @@
+package Mar26th;
+
+import java.util.Scanner;
+
+public class Report6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int num1;
+		num1 = scanner.nextInt();
+		for (int i = 1; i < num1; i++) {
+			if (num1 % i == 0) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
