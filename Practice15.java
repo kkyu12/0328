4e880a02e177ae39c23133cb18a85e4f4a574dd7
kkyu12 <101681296+kkyu12@users.noreply.github.com>
2022-03-28 18:37:42 +0900

    for (Loof)

---
+++ Practice15.java
@@ -0,0 +1,14 @@
+package Mar28th;
+
+public class Practice15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub.
+		int i = 0;
+		for (i = 2; i < 10; i++) {
+			System.out.println(3 + "X" + i + " = " + (3 * i));
+		}
+
+	}
+
+}
